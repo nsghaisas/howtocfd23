fe01710a5080c97b090e362d431a9907db84f0cf
Niranjan Ghaisas <22302124+nsghaisas@users.noreply.github.com>
2023-06-19 05:59:37 +0530

    Adding a file to print integers

---
+++ day0/addsum.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main()
+{
+
+  int i, n=10;
+
+  printf("\nHello World\n");
+
+  printf("The first %d integers are:\n", n);
+  for(i=0; i<n; i++)
+    printf("%d\n", i);
+
+  return 0;
+}
+
